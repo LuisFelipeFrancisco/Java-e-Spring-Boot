ada2e35c5882f3122991e090b93183d17647980a
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-23 12:22:15 -0300

    feat: Pagination and sorting

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -6,10 +6,12 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 
 @RestController
@@ -26,10 +28,8 @@ public class MedicosController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar(){
-        return repo.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, page = 0, sort = {"nome"}) Pageable paginacao){
+        return repo.findAll(paginacao).map(DadosListagemMedico::new);
     }
-
-
 
 }
