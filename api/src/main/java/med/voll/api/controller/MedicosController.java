414de43863e7d907b88e196b3e9a49b7d1865e32
Luis Felipe Francisco <luis.ferreira36@fatec.sp.gov.br>
2024-04-23 11:52:28 -0300

    feat: Listing

--- api/src/main/java/med/voll/api/controller/MedicosController.java
+++ api/src/main/java/med/voll/api/controller/MedicosController.java
@@ -6,10 +6,10 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -26,14 +26,9 @@ public class MedicosController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
-        return repo.findAll(paginacao).map(DadosListagemMedico::new);
-    }
-    /*
     public List<DadosListagemMedico> listar(){
         return repo.findAll().stream().map(DadosListagemMedico::new).toList();
     }
-    */
 
 
 
